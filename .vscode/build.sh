869821bcda7ab8988daf90fae9c33d004b95958b
twstagg <tristinstagg@protonmail.com>
2023-02-08 17:34:55 -0600

    Stop evaluating labels... add Linux build env (VSCode)

---
+++ .vscode/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm ./1.4/Assemblies/ATR\ Implants\ Patch.dll
+
+dotnet build .vscode
--- Source/ATR Implants Patch/PatchImplants.cs
+++ Source/ATR Implants Patch/PatchImplants.cs
@@ -7,7 +7,7 @@ namespace ATR_Implants_Patch
     [StaticConstructorOnStartup]
     public static class PatchImplants
     {
-        public static string ATR_BrainShortLabel = "artificial brain";
+        public static BodyPartDef ATR_ArtificialBrain = DefDatabase<BodyPartDef>.GetNamed("ATR_ArtificialBrain");
         static PatchImplants()
         {
             new Harmony("twsta.ATRImplantsPatch").PatchAll();
@@ -17,19 +17,17 @@ namespace ATR_Implants_Patch
         {
             static void Prefix(ref Pawn user, CompUseEffect_InstallImplant __instance, out BodyPartDef __state)
             {
-                __state = __instance.Props.bodyPart;                                                                                        // Save original human brain BodyPartDef for later.
-                BodyPartDef getPawnBrain = user.health.hediffSet.GetBrain().def;                                                            // What is pawn brain?
-                if (getPawnBrain.LabelShort == ATR_BrainShortLabel)                                                                         // If pawn has artificial brain,
+                __state = __instance.Props.bodyPart;                                                                                        // Save original human "Brain" BodyPartDef target to reset later.
+                if (user.health.hediffSet.GetBrain().def == ATR_ArtificialBrain)                                                            // If source of the implant user's consciousness is "ATR_ArtificialBrain" BodyPartDef,
                 {
-                    __instance.Props.bodyPart = getPawnBrain;                                                                               // then replace target with the BodyPartDef from the Android, which contains artificial brain.
+                    __instance.Props.bodyPart = user.health.hediffSet.GetBrain().def;                                                       // then replace BodyPartDef target with the Android user's BodyPartDef.
                 }
             }
             static void Postfix(ref Pawn user, CompUseEffect_InstallImplant __instance, BodyPartDef __state)
             {
-                BodyPartDef getPawnBrain = user.health.hediffSet.GetBrain().def;                                                            // What is pawn brain?
-                if (getPawnBrain.LabelShort == ATR_BrainShortLabel)                                                                         // If pawn has artificial brain,
+                if (user.health.hediffSet.GetBrain().def == ATR_ArtificialBrain)                                                            // If source of the implant user's consciousness is "ATR_ArtificialBrain" BodyPartDef,
                 {
-                    __instance.Props.bodyPart = __state;                                                                                    // then restore the original BodyPartDef, __state, which contains human brain.
+                    __instance.Props.bodyPart = __state;                                                                                    // then restore the original human "Brain" BodyPartDef target from __state.
                 }
             }
         }
@@ -38,12 +36,11 @@ namespace ATR_Implants_Patch
         {
             public static void Postfix(ref Pawn p, ref string failReason, ref bool __result)
             {
-                BodyPartDef getPawnBrain = p.health.hediffSet.GetBrain().def;                                                               // Set strings that the game expects to be used for the context menu
-                string noBodyPart = (string)("InstallImplantNoBodyPart".Translate() + ": " + "brain");
+                string noBodyPart = (string)("InstallImplantNoBodyPart".Translate() + ": " + "brain");                                      // Vanilla string expected by context menu (taken from decompiled code)
                 if (__result == false)                                                                                                      // If CanBeUsedBy() returned false,
                 {
-                    if (getPawnBrain.LabelShort == ATR_BrainShortLabel && failReason == noBodyPart)                                         // and pawn is Android or is failing due to "stock" limitations,
-                    {
+                    if (p.health.hediffSet.GetBrain().def == ATR_ArtificialBrain && failReason == noBodyPart)                               // and source of the pawn's consciousness is "ATR_ArtificialBrain" BodyPartDef,
+                    {                                                                                                                       // or is failing due to not having a human "Brain" BodyPartDef,
                         __result = true;                                                                                                    // allow the Android pawn to install Mechlink implant.
                     }
                 }
